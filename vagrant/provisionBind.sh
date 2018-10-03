c590a393517d3bb59ae50d5c685cf4b80e2014e5
Gabriel Viel <g.viel@net4up.com>
2018-10-03 23:34:34 +0200

    log bind

--- vagrant/provisionBind.sh
+++ vagrant/provisionBind.sh
@@ -6,3 +6,6 @@ echo "Provisioning bind virtual machine..."
 sudo su -
 apt-get update -y
 apt-get install bind9 dnsutils -y
+mkdir /var/log/bind
+chown bind:bind /var/log/bind
+echo 'include "/bind/named.conf.log";' >> /etc/bind/named.conf
