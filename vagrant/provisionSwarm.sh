1a82f4f09a7042b578da680132d7f41018c5740c
Gabriel Viel <g.viel@net4up.com>
2018-10-12 16:08:27 +0200

    consul cluster

--- vagrant/provisionSwarm.sh
+++ vagrant/provisionSwarm.sh
@@ -17,4 +17,10 @@ apt-get install docker-ce -y
 # Swarm 02/03
 # docker swarm join --token SWMTKN-1-4b0yzdn30rx7qopbor2j0frbdx110a9kxfurtpqtp4modi80so-25gg795z2y85tj47j7hv56w90 192.168.10.1:2377
 
-# docker network create --subnet 10.0.0.0/8 --driver overlay saas
+# Overlay Network
+# docker network create --subnet 1.0.0.0/8 --driver overlay saas
+
+# Consul Server
+# docker service create --name consul --network saas --mode global --env CONSUL_BIND_INTERFACE=eth0 --env CONSUL_CLIENT_INTERFACE=eth0 consul agent -server -bootstrap-expect=3  -retry-join=tasks.consul
+#
+# Test : curl http://consul:8500/v1/agent/members?pretty (from within a container, docker ps thent docker exec -it <ID> /bin/sh)
