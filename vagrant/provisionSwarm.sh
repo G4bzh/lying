2c6ef1912d398b9854d40dfe37e794f12f06390d
Gabriel Viel <g.viel@net4up.com>
2018-10-28 11:33:29 +0100

    try to auto vagrant swarm

--- vagrant/provisionSwarm.sh
+++ vagrant/provisionSwarm.sh
@@ -10,3 +10,27 @@ curl -fsSL https://download.docker.com/linux/debian/gpg | apt-key add -
 add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
 apt-get update -y
 apt-get install docker-ce -y
+mkdir -p ~/.ssh 2>/dev/null
+cp /vagrant/id_rsa* ~/.ssh
+chmod 600 ~/.ssh/id_rsa
+cat ~/.ssh/id_rsa.pub >> ~/.ssh/authorized_keys
+if [ "$HOSTNAME" == "swarm01" ]; then
+  docker swarm init --advertise-addr 192.168.10.1
+  docker swarm join-token -q worker > /vagrant/swarm_token
+  docker node update swarm01 --label-add consul=1
+  ping -c 3 192.168.10.1
+fi
+if [ "$HOSTNAME" == "swarm02" ]; then
+  ping -c 3 192.168.10.1
+  ssh-keyscan -p 2201 -H 192.168.10.1 >> ~/.ssh/known_hosts
+  ssh 192.168.10.1 -p 2201 ping -c 192.168.10.2
+  docker swarm join --token `echo -n $(head /vagrant/swarm_token)` 192.168.10.1:2377
+  docker node update swarm02 --label-add consul=2
+fi
+if [ "$HOSTNAME" == "swarm03" ]; then
+  ping -c 3 192.168.10.1
+  ssh-keyscan -p 2201 -H 192.168.10.1 >> ~/.ssh/known_hosts
+  ssh 192.168.10.1 -p 2201 ping -c 192.168.10.2
+  docker swarm join --token `echo -n $(head /vagrant/swarm_token)` 192.168.10.1:2377
+  ssh 192.168.10.1 -p 2201 docker node update swarm03 --label-add consul=3
+fi
