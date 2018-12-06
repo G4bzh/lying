34b5b2d14995a262ee28d35be6845cea1aa93535
Gabriel Viel <g.viel@net4up.com>
2018-12-06 20:35:07 +0100

    add router

--- services/ui/app.js
+++ services/ui/app.js
@@ -1,10 +1,24 @@
 Vue.use(VueMaterial.default)
+Vue.use(VueRouter);
 
 import SignIn from './signin.vue.js';
 import SignUp from './signup.vue.js';
 
 
+const routes = [{
+  path: '/signin',
+  component: SignIn
+}, {
+  path: '/signup',
+  component: SignUp
+}];
+
+const router = new VueRouter({
+    routes // short for `routes: routes`
+})
+
 var app = new Vue({
+  router,
   el: '#app',
   components : {
     SignIn,
