074d980b86b2c6984bea6063e54dd9cbc5d0f1a2
Gabriel Viel <g.viel@net4up.com>
2018-12-14 12:26:03 +0100

    cards

--- services/ui/mydashboard.vue.js
+++ services/ui/mydashboard.vue.js
@@ -2,7 +2,42 @@ export default {
   name: "MyDashboard",
   template: `
   <div>
-      Hello {{username}}
+    <md-card md-with-hover>
+
+        <md-card-header>
+          <div class="md-title">Hello</div>
+        </md-card-header>
+
+        <md-card-content>
+          {{username}}
+        </md-card-content>
+
+    </md-card>
+
+    <md-card md-with-hover>
+
+        <md-card-header>
+          <div class="md-title">Your Zones</div>
+        </md-card-header>
+
+        <md-card-content>
+          10
+        </md-card-content>
+
+    </md-card>
+
+    <md-card md-with-hover>
+
+        <md-card-header>
+          <div class="md-title">Requests</div>
+        </md-card-header>
+
+        <md-card-content>
+          1234567
+        </md-card-content>
+
+    </md-card>
+
   </div>
   `,
   data: function() {
